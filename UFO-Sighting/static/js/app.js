8612851e5b882d3aaaa2f1082754f140180748b5
mosal2000 <mohammad.ronosentono@gmail.com>
2019-03-21 22:56:12 -0400

    improve UFO Sighting

--- UFO-Sighting/static/js/app.js
+++ UFO-Sighting/static/js/app.js
@@ -4,21 +4,23 @@ var tableData = data;
 var tableBody = d3.select("tbody");
 
 String.prototype.toProperCase = function () {
-    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+	return this.replace(/\w\S*/g, function (txt) {
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	});
 };
 
 function builtTablePage(sightingData) {
 
 	tableBody.html("");
 
-	sightingData.forEach(function(row) {
+	sightingData.forEach(function (row) {
 		var rowData = Object.values(row)
 		// console.log(rowData);
 
 		var insertRow = tableBody.append("tr");
 		var colNum = 0;
 
-		rowData.forEach(function(rowColData){
+		rowData.forEach(function (rowColData) {
 			// console.log(rowColData);
 			colNum++;
 
@@ -35,28 +37,46 @@ function builtTablePage(sightingData) {
 	});
 }
 
+var filters = {};
+
+function updateFilters() {
+
+	// Save the element, value, and id of the filter that was changed
+	var changedElement = d3.select(this).select("input");
+	var elementValue = changedElement.property("value");
+	var filterId = changedElement.attr("id");
+
+	// If a filter value was entered then add that filterId and value
+	// to the filters list. Otherwise, clear that filter from the filters object
+	if (elementValue) {
+		filters[filterId] = elementValue;
+	} else {
+		delete filters[filterId];
+	}
+
+	// Call function to apply all filters and rebuild the table
+	filterTable();
+
+}
+
 var filterBtn = d3.select("#filter-btn");
 
 
-filterBtn.on("click", function() {
+filterBtn.on("click", function () {
 	d3.event.preventDefault();
 
-	var inputElement = d3.select("#datetime");
-	var inputValue = inputElement.property("value");
+	let filteredData = tableData
 
-	console.log(inputValue);
+	// Loop through all of the filters and keep any data that
+	// matches the filter values
+	Object.entries(filters).forEach(([key, value]) => {
+		filteredData = filteredData.filter(row => row[key].toUpperCase() === value.toUpperCase());
+	});
 
-	if (inputValue == "") {
-		console.log("inputValue is Empty");
-		builtTablePage(tableData);
-
-	} else {
-		console.log("inputValue has value");
-
-		var filteredData = tableData.filter(rowData => rowData.datetime === inputValue);
-
-		builtTablePage(filteredData);
-	}
+	builtTablePage(filteredData);
 });
+
+// Attach an event to listen for changes to each filter
+d3.selectAll(".filter").on("change", updateFilters);
 
 builtTablePage(tableData);
